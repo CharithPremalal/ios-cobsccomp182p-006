d304aa07aa57a2890ddee552c3111818e0f46c99
Charith Lakshitha <charithlakshitha@outlook.com>
2020-02-19 23:59:27 +0530

    Creating main view controllers

---
+++ charith-cobsccomp182p-006/charith-cobsccomp182p-006/View Controllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  charith-cobsccomp182p-006
+//
+//  Created by Charith Lakshitha on 2/19/20.
+//  Copyright © 2020 Charith Lakshitha. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
