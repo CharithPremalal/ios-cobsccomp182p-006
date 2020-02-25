f7bce475ec51662d053d0862e4c5cb79d850d8f2
Charith Lakshitha <charithlakshitha@outlook.com>
2020-02-25 23:13:56 +0530

    Pod error fix

---
+++ charith-cobsccomp182p-006/charith-cobsccomp182p-006/View Controllers/ProfileViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ProfileViewController.swift
+//  charith-cobsccomp182p-006
+//
+//  Created by Charith Lakshitha on 2/25/20.
+//  Copyright © 2020 Charith Lakshitha. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+
+
+class ProfileViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        checkLoggedInUserStatus()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    fileprivate func checkLoggedInUserStatus(){
+        if Auth.auth().currentUser == nil{
+            
+            DispatchQueue.main.async {
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
+                self.present(vc, animated: true, completion: nil)
+                
+                return
+            }
+        }else{
+            
+          print("user Logged")
+        }
+        
+    }
+    
+    @IBAction func SignOutTapped(_ sender: Any) {
+        do{
+            try Auth.auth().signOut()
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
+            self.present(vc, animated: true, completion: nil)
+            
+        }catch let err{
+            
+            print("Failed to sign out with error",err)
+            
+            
+        }
+
+    }
+    
+
+}
