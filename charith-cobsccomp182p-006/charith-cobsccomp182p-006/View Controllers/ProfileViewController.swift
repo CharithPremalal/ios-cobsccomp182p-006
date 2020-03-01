a77293de1f05d90e76548460996754ac56efe350
Charith Lakshitha <charithlakshitha@outlook.com>
2020-03-01 22:43:00 +0530

    Profile Complete

--- charith-cobsccomp182p-006/charith-cobsccomp182p-006/View Controllers/ProfileViewController.swift
+++ charith-cobsccomp182p-006/charith-cobsccomp182p-006/View Controllers/ProfileViewController.swift
@@ -9,14 +9,25 @@
 import UIKit
 import FirebaseAuth
 import LocalAuthentication
+import Firebase
+import FirebaseStorage
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var FirstName: UILabel!
+    @IBOutlet weak var LastName: UILabel!
+    @IBOutlet weak var Email: UILabel!
+    @IBOutlet weak var Phone: UILabel!
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         checkLoggedInUserStatus()
+        
+
         // Do any additional setup after loading the view.
     }
     
@@ -46,10 +57,33 @@ class ProfileViewController: UIViewController {
             }
         }else{
             BioLogin()
+            getEventOwnerName()
         }
     }
     
-    
+    func getEventOwnerName(){
+        
+        guard let uid = Auth.auth().currentUser?.uid else{ return }
+        print(uid)
+        let db = Firestore.firestore()
+        let docRef = db.collection("users").document(uid)
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                print("Document data: \(dataDescription)")
+                self.FirstName.text = (document.get("firstname") as! String)
+                self.LastName.text = (document.get("lastname") as! String)
+          //      self.Email.text = (document.get("email") as! String)
+                self.Phone.text = (document.get("phone") as! String)
+                let profile = (document.get("profilimg") as! String)
+                self.profileImg.kf.setImage(with: URL(string: profile), placeholder: nil, options: [.transition(.fade(0.7))], progressBlock: nil)
+
+            } else {
+                print("Document does not exist")
+            }
+        }
+    }
     
     @IBAction func LogoutTapped(_ sender: Any) {
         do{
